7ea98aff64dff0ca5194c78d7a457ca99baae3c5
Hirwa <hirwanshutiflx@gmail.com>
2022-02-03 22:48:35 +0530

    Added nCr

---
+++ Recursion/nCr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int C(int n, int r)
+{
+    if (r == 0 || n == r)
+        return 1;
+    else
+        return C(n - 1, r - 1) + C(n - 1, r);
+}
+
+int main()
+{
+    int n = 4, r = 2;
+    cout << C(n, r) << endl;
+    cin.get();
+}
